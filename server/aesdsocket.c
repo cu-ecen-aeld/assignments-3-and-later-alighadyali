c5635d2aa3efafac9be8cc5e68d2852dfbb0d762
alighadyali <alighadyali@github.com>
2024-05-12 17:19:45 -0700

    assignment5-1-complete

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -38,55 +38,57 @@ int init_signal_handler()
 
 int send_file_contents_over_socket(socket_context *socket_context)
 {
-    ssize_t bytesRead = 0;
-    FILE *dataFile = fopen(FILENAME, "r");
-    if (!dataFile)
+    ssize_t bytes_read = 0;
+    FILE *data_file = fopen(FILENAME, "r");
+    if (!data_file)
     {
         syslog(LOG_ERR, "Error opening data file.");
         return -1;
     }
 
     char buffer[BUFFERSIZE];
-    while ((bytesRead = fread(buffer, 1, sizeof(buffer), dataFile)))
+    while ((bytes_read = fread(buffer, 1, sizeof(buffer), data_file)))
     {
         if ((send(
-                *socket_context->p_accept_connection_fd, buffer, bytesRead, 0)) !=
-            bytesRead)
+                *socket_context->p_accept_connection_fd,
+                buffer,
+                bytes_read,
+                0)) != bytes_read)
         {
             syslog(
                 LOG_ERR, "Error sending file contents over socket connection.");
             return -1;
         }
     }
-    fclose(dataFile);
+    fclose(data_file);
     return 0;
 }
 
 int receive(socket_context *p_socket_context)
 {
-    FILE *dataFile = fopen(FILENAME, "a");
-    if (!dataFile)
+    FILE *data_file = fopen(FILENAME, "a");
+    if (!data_file)
     {
         syslog(LOG_ERR, "Unable to open file for writing.");
         return -1;
     }
 
-    size_t bytesReceived = 0;
+    size_t bytes_received = 0;
     char buffer[BUFFERSIZE];
 
-    while ((bytesReceived = recv(
+    while ((bytes_received = recv(
                 *p_socket_context->p_accept_connection_fd,
                 buffer,
                 sizeof(buffer),
                 0)) > 0)
     {
-        fwrite(buffer, 1, bytesReceived, dataFile);
-        if (memchr(buffer, '\n', bytesReceived) != NULL)
+        fwrite(buffer, 1, bytes_received, data_file);
+        if (memchr(buffer, '\n', bytes_received) != NULL)
         {
             break;
         }
     }
-    fclose(dataFile);
+    fclose(data_file);
 
     // Send back the contents of the file over the socket connection.
     send_file_contents_over_socket(p_socket_context);
@@ -105,18 +107,18 @@ int accept_connections(socket_context *p_socket_context)
         }
         *p_socket_context->p_sock_addr_storage_size =
             sizeof(*p_socket_context->p_sock_addr_storage);
-        int acceptedConnectionFd = accept(
+        int accepted_connection_fd = accept(
             *p_socket_context->p_socket_fd,
             (struct sockaddr *)p_socket_context->p_sock_addr_storage,
             p_socket_context->p_sock_addr_storage_size);
-        if (acceptedConnectionFd == -1)
+        if (accepted_connection_fd == -1)
         {
-            syslog(LOG_ERR, "Eroor accepting connection.");
+            syslog(LOG_ERR, "Error accepting connection.");
             continue;
         }
         else
         {
-            p_socket_context->p_accept_connection_fd = &acceptedConnectionFd;
+            p_socket_context->p_accept_connection_fd = &accepted_connection_fd;
             syslog(LOG_USER, "Accepted connection.");
 
             receive(p_socket_context);
@@ -125,17 +127,17 @@ int accept_connections(socket_context *p_socket_context)
     return 0;
 }
 
-int cleanup(socket_context *pSocketContext)
+int cleanup(socket_context *p_socket_context)
 {
-    freeaddrinfo(pSocketContext->p_addr_info);
-    pSocketContext->p_addr_info = NULL;
-    pSocketContext->p_hints = NULL;
-    pSocketContext->p_sock_addr_storage = NULL;
-    pSocketContext->p_sock_addr_storage_size = NULL;
-    pSocketContext->p_socket_fd = NULL;
-    pSocketContext->p_accept_connection_fd = NULL;
+    freeaddrinfo(p_socket_context->p_addr_info);
+    p_socket_context->p_addr_info = NULL;
+    p_socket_context->p_hints = NULL;
+    p_socket_context->p_sock_addr_storage = NULL;
+    p_socket_context->p_sock_addr_storage_size = NULL;
+    p_socket_context->p_socket_fd = NULL;
+    p_socket_context->p_accept_connection_fd = NULL;
 
-    free(pSocketContext);
+    free(p_socket_context);
     remove(FILENAME);
     return 0;
 }
