528e97168b4090d9d64c60a34977de72f0387fd0
alighadyali <alighadyali@github.com>
2024-06-04 18:51:10 -0700

    timestamp

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -216,7 +216,7 @@ int daemonize(int *p_socket_fd, struct sockaddr_storage *p_sock_addr_storage)
     dup2(nullFd, 1);
 
     // Start the timestamp thread.
-    // initTimestampHandler();
+    initTimestampHandler();
 
     // Begin accepting connections and exchanging data.
     initConnectionHandler(p_socket_fd, p_sock_addr_storage);
